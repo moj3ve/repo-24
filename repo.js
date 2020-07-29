3b0a49156aff7ad1e18317f38384c4a946a32573
taki <49000471+74k1@users.noreply.github.com>
2020-07-30 00:04:15 +0200

    🍳 testing with no packages at all

--- repo.js
+++ repo.js
@@ -2,9 +2,9 @@ exports.name = 'taki'
 exports.description = "taki's repository"
 
 exports.packages = [
-	'https://github.com/74k1/repo/releases/download/KindaStockpp-v0.3/com.taki.KindaStockpp_0.3_iphoneos-arm.deb',
+	// 'https://github.com/74k1/repo/releases/download/KindaStockpp-v0.3/com.taki.KindaStockpp_0.3_iphoneos-arm.deb',
 ]
 
 exports.icons = {
-	KindaStockpp: 'assets/KindaStockpp/icon.png',
+	// KindaStockpp: 'assets/KindaStockpp/icon.png',
 }
