b642c930e1261d40c7e2ca2f510dc85ceb634979
taki <49000471+74k1@users.noreply.github.com>
2020-07-29 16:05:36 +0200

    🍗 hoping this works

---
+++ pages/KindaStockpp.js
@@ -0,0 +1,9 @@
+import Depiction from '../components/Depiction'
+
+export default function KindaStockpp() {
+	return (
+		<Depiction name="KindaStockpp" github="https://github.com/74k1/repo/releases/tag/KindaStockpp-v0.5">
+			KindaStock's Style for Icons that aren't in KindaStock (yet?)
+		</Depiction>
+	)
+}
