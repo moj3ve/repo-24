69c9cf4c8aa39c15f8505fdbb06e89551684665d
taki <49000471+74k1@users.noreply.github.com>
2020-07-30 02:14:15 +0200

    👼

--- repo.js
+++ repo.js
@@ -2,6 +2,7 @@ exports.name = 'taki'
 exports.description = "taki's repository"
 
 exports.packages = [
+	'https://repo.litten.love/debs/sh.litten.butterfly_1.2_iphoneos-arm.deb',
 	// 'https://github.com/74k1/repo/releases/download/KindaStockpp-v0.1/com.taki.KindaStockpp_0.1_iphoneos-arm.deb',
 ]
 
