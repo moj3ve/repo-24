34ac33e2170117b1fe84805924d8ffb2d7f0c02e
taki <49000471+74k1@users.noreply.github.com>
2020-08-05 00:35:10 +0200

    🎭

--- repo.js
+++ repo.js
@@ -4,7 +4,7 @@ exports.description = "taki's repository"
 exports.packages = [
 	'https://github.com/74k1/repo/releases/download/Aether-v0.0.1/moe.taki.aether_0.0.1_iphoneos-arm.deb',
 	'https://github.com/74k1/repo/releases/download/TakiUI-v1.0/moe.taki.takiui_1.0_iphoneos-arm.deb',
-	'https://github.com/74k1/repo/releases/download/KindaStockpp-v0.1/moe.taki.KindaStockpp_0.1_iphoneos-arm.deb.deb',
+	'https://github.com/74k1/repo/releases/download/KindaStockpp-v0.1/moe.taki.KindaStockpp_0.1_iphoneos-arm.deb',
 ]
 
 exports.icons = {
