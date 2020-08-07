cbedcdd30bc267c28f57a85d113d4e10c12aebd0
taki <49000471+74k1@users.noreply.github.com>
2020-08-07 03:33:58 +0200

    🎠 Aether v1.0 release

--- pages/Aether.js
+++ pages/Aether.js
@@ -2,8 +2,8 @@ import Depiction from '../components/Depiction'
 
 export default function Aether() {
 	return (
-		<Depiction name="Aether" github="https://github.com/74k1/repo/tree/Aether-v0.0.1">
-			a theme
+		<Depiction name="Aether" github="https://github.com/74k1/repo/tree/Aether-v1.0">
+			a theme (currently with +120 icons)
 		</Depiction>
 	)
 }
--- repo.js
+++ repo.js
@@ -2,6 +2,7 @@ exports.name = 'taki'
 exports.description = "taki's repository"
 
 exports.packages = [
+	'https://github.com/74k1/repo/releases/download/Aether-v1.0/moe.taki.aether_1.0_iphoneos-arm.deb',
 	'https://github.com/74k1/repo/releases/download/Aether-v0.1/moe.taki.aether_0.1_iphoneos-arm.deb',
 	'https://github.com/74k1/repo/releases/download/TakiUI-v1.0/moe.taki.takiui_1.0_iphoneos-arm.deb',
 	'https://github.com/74k1/repo/releases/download/KindaStockpp-v0.1/moe.taki.KindaStockpp_0.1_iphoneos-arm.deb',
